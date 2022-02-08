595a37f93a0904b669ca3f44c52f441900e0b2ef
SanalMohan664 <70734109+SanalMohan664@users.noreply.github.com>
2022-02-08 15:42:46 +0530

    Create pgm3.py

---
+++ Python/CO4/pgm3.py
@@ -0,0 +1,18 @@
+class Rectangle:
+    def __init__(self,length,breadth):
+        self.length=length
+        self.breadth=breadth
+        self.area=length*breadth
+    def __lt__(self,m):
+        return self.area<m.area
+r1=Rectangle(50,20)
+r2=Rectangle(20,30)
+if r1<r2:
+    print("Rectangle one has largest area")
+else:
+    print("Rectangle two has largest area")
+© 2022 GitHub, Inc.
+Terms
+Privacy
+Security
+St
